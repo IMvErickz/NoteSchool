f65d3eda9cb38b35064f98d4b8150865d22e78e9
IMvErickz <erickspy2003@gmail.com>
2022-08-24 17:27:58 -0300

    adde the LocalStorage of name

---
+++ src/index.js
@@ -0,0 +1,8 @@
+let button = document.getElementById("button")
+    .addEventListener("click", () => {
+        let name = document.getElementById("name").value
+        swal("Sucesso", "Seu nome está salvo", "success")
+            .then(() => {
+            localStorage.setItem("Nome", name)
+        })
+})
