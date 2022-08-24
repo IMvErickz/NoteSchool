e7458c7ca77fb12dc724ce90e142898b8e97adf4
IMvErickz <erickspy2003@gmail.com>
2022-08-24 18:27:35 -0300

    added the function of save the check list

--- src/to-do.js
+++ src/to-do.js
@@ -4,3 +4,15 @@ window.onload = function(){
 
     localName.innerHTML = recName
 }
+
+
+let button = document.getElementById('button')
+    .addEventListener("click", () => {
+        let coment = document.getElementById("description").value
+        localStorage.setItem("Check", coment)
+
+        let span = document.getElementById("span")
+        let recComent = localStorage.getItem("Check")
+
+        span.innerHTML = recComent
+})
