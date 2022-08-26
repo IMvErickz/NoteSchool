a596b3b0914bb2409376ce4e7843aa5c74bbf4c0
unknown <erickspy2003@gmail.com>
2022-08-26 17:36:45 -0300

    added the function for enter button for save name in the LocalStorage

--- src/index.js
+++ src/index.js
@@ -5,4 +5,18 @@ let button = document.getElementById("button")
             .then(() => {
             localStorage.setItem("Nome", name)
             })
-})
+    })
+
+    document.addEventListener('keypress', function(e){
+  if(e.which == 13){
+    enviaForm()
+  }
+}, false)
+
+function enviaForm() {
+    let name = document.getElementById("name").value
+swal("Sucesso", "Seu nome está salvo", "success")
+    .then(() => {
+    localStorage.setItem("Nome", name)
+    })
+}
