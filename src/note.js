24fb82366d2f6c854b3689d742a0298ff16d4e38
unknown <erickspy2003@gmail.com>
2022-08-30 17:33:07 -0300

    added the bootstrap framework and a new modal

--- src/note.js
+++ src/note.js
@@ -7,45 +7,17 @@ window.onload = function(){
 
 let button = document.getElementById("button")
   .addEventListener("click", () => {
-  const switchModal = () => {
-const modal = document.querySelector('.modal')
-const actualStyle = modal.style.display
-if(actualStyle == 'block'){
-  modal.style.display = 'none'
-}else{
-  modal.style.display = 'block'
-
-}
-}
-
-const btn = document.querySelector('.buttonCard')
-.addEventListener("click", switchModal)
-
-window.onclick = function(event){
-  const modal = document.querySelector('.modal')
-  if(event.target == modal){
-    switchModal()
-  }
-}
-  })
-
-let saved = document.getElementById("butModal")
-  .addEventListener("click", () => {
     let note = {
-      Titulo:  document.getElementById("tittle").value,
-      Comentario:  document.getElementById("coment").value
-    }
-
-    localStorage.setItem("save", JSON.stringify(note))
-
-    let tittle = document.getElementById("buttonCard")
-    let coment = document.getElementById("descrition")
-    let recSave = JSON.parse(localStorage.getItem("save"))
+  Titulo:  document.getElementById("tittle").value,
+  Comentario:  document.getElementById("coment").value
+}
+localStorage.setItem("save", JSON.stringify(note))
+let tittle = document.getElementById("buttonCard")
+let coment = document.getElementById("descrition")
+let recSave = JSON.parse(localStorage.getItem("save"))
     
-    tittle.innerHTML = recSave.Titulo
-    coment.innerHTML = recSave.Comentario
-
-document.getElementByClassName
+tittle.innerHTML = recSave.Titulo
+coment.innerHTML = recSave.Comentario
   })
 
 let excluid = document.getElementById("exclude")
@@ -57,3 +29,7 @@ let excluid = document.getElementById("exclude")
     
   })
 
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+  });
